584f16b59f7f0508f92cde6eaeaeffdb9c41dc7b
Luis Rodriguez <your.email@example.com>
2026-06-14 22:46:45 +0200

    Fix Plymouth installer

--- os/plymouth/install.sh
+++ os/plymouth/install.sh
@@ -10,15 +10,9 @@ sudo cp splash.png "$THEME_DIR/"
 sudo cp dorian.plymouth "$THEME_DIR/"
 sudo cp dorian.script "$THEME_DIR/"
 
-sudo update-alternatives --install 
-/usr/share/plymouth/themes/default.plymouth 
-default.plymouth 
-"$THEME_DIR/dorian.plymouth" 
-100
+sudo update-alternatives --install /usr/share/plymouth/themes/default.plymouth default.plymouth "$THEME_DIR/dorian.plymouth" 100
 
-sudo update-alternatives --set 
-default.plymouth 
-"$THEME_DIR/dorian.plymouth"
+sudo update-alternatives --set default.plymouth "$THEME_DIR/dorian.plymouth"
 
 sudo update-initramfs -u
 
